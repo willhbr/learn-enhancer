afd2e1c023e4737a5d5bc51fcf510d6e7702de82
Will Richardson <william.hamish@gmail.com>
2015-07-17 21:12:26 +1200

    Fix for non-previewable files

--- background.js
+++ background.js
@@ -1,12 +1,22 @@
 chrome.webRequest.onHeadersReceived.addListener(function(details) {
+  var contentTypeIndex = -1;
+  var contentDispositionIndex = -1;
   for(var i = 0; i < details.responseHeaders.length; i++) {
+    console.log(details.responseHeaders[i].name);
+    console.log(details.responseHeaders[i].value);
     if(details.responseHeaders[i].name.toLowerCase() == 'content-type') {
       if(details.responseHeaders[i].value.toLowerCase() == 'application/x-forcedownload') {
-        details.responseHeaders[i].value = 'application/pdf';
+        contentTypeIndex = i;
       }
     } else if(details.responseHeaders[i].name.toLowerCase() == 'content-disposition') {
-      details.responseHeaders[i].value = 'inline';
+      contentDispositionIndex = i;
     }
+  }
+  var match = details.responseHeaders[contentDispositionIndex].toLowerCase().match(/filename=".*?\.(pdf|jpg|png|jpeg)"/)
+  if(match != null) {
+    var extension = match[1];
+    details.responseHeaders[contentDispositionIndex].value = 'inline';
+    details.responseHeaders[contentTypeIndex].value = 'application/' + extension;
   }
   return {responseHeaders:details.responseHeaders};
 }, {urls: ['http://learn.canterbury.ac.nz/pluginfile.php/*']}, ['blocking', 'responseHeaders']);
