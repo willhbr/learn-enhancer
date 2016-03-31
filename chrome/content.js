8fa9d42c3a529121b224a0ea013ed79c63803259
Will Richardson <william.hamish@gmail.com>
2016-03-31 20:47:11 +1300

    Added april fools stuff

--- chrome/content.js
+++ chrome/content.js
@@ -1,3 +1,38 @@
+function aprilFools() {
+  function add(content, front) {
+    if(front) {
+      document.body.innerHTML = content + document.body.innerHTML;
+    } else {
+      document.body.innerHTML += content;
+    }
+  }
+  add('<h1 style="font-size:100px;text-align:center">&larr; APRIL FOOLS &rarr;</h1>', true);
+  [
+    function() {
+      add("<style>*:not(:hover) { font-family: 'Wingdings' !important; }</style>", true);
+    },
+    function() {
+      var deg = (Math.random() * 140) + 20;
+      add('<style>div{transform:rotate(' + deg + 'deg);}</style>');
+    },
+    function() {
+      add("<style>div {animation: marquee 3s linear infinite;} @keyframes marquee {0%   {margin: 0px;}25%  {margin: 5px;}50%  {margin: 10px;}75%  {margin: 5px;}100% {margin: 0px;}}</style>");
+    },
+    function() {
+      var styles = ".block {animation: spin-baby 6s linear infinite;} @keyframes spin-baby {from {transform: rotate(0deg);} to {transform: rotate(360deg);}}";
+      styles += ".forumpost {animation: spin-baby-rev 12s linear infinite;} @keyframes spin-baby-rev {from {transform: rotate(360deg);} to {transform: rotate(0deg);}}"
+      add("<style>" + styles + "</style>");
+    },
+    function() {
+      var styles = ".block div {animation: hue-baby 1s linear infinite;} @keyframes hue-baby {0% {background-color: #990886;} 50% {background-color: #0F990D;} 100% {background-color: #990886;}}";
+      add("<style>" + styles + "</style>");
+    },
+    function() {
+      window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    }
+  ][Math.floor(Math.random() * 6)]();
+}
+
 DomReady.ready(function() {
   var thing = document.getElementById("resourceobject");
   if(thing != null) {
@@ -8,6 +43,17 @@ DomReady.ready(function() {
       window.location.replace(frame.src);
     }
   }
+  
+  var date = new Date();
+  if(date.getDate() == 1 && date.getMonth() == 3) {
+    var rand = Math.random();
+    if(rand < 0.15) {
+      aprilFools();
+    }
+  } else {
+    console.log(date);
+  }
+  
   // Lol, James..
   if(document.URL.indexOf("blackboard.vuw.ac.nz/") != -1) {
     var nameLink = document.getElementById("global-nav-link");
