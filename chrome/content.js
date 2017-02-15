40ba5b190165d472fa90152d7fffb552d5e8cac5
Will Richardson <william.hamish@gmail.com>
2017-02-15 13:56:32 +1300

    Updated for new Learn theme

--- chrome/content.js
+++ chrome/content.js
@@ -1,16 +1,16 @@
-DomReady.ready(function() {
+DomReady.ready(() => {
   var thing = document.getElementById("resourceobject");
   if(thing != null) {
-    window.location.replace(thing.data);
+    return window.location.replace(thing.data);
   } else {
     var frame = document.getElementById("cleanSlate");
     if(frame != null) {
-      window.location.replace(frame.src);
+      return window.location.replace(frame.src);
     }
   }
   
   var date = new Date();
-  if(date.getDate() == 1 && date.getMonth() == 3) {
+  if (date.getDate() == 1 && date.getMonth() == 3) {
     var rand = Math.random();
     if(rand < 0.15) {
       aprilFools();
@@ -24,13 +24,11 @@ DomReady.ready(function() {
     nameLink.innerHTML = oldHTML.replace("James Wright", '"Will is Awesome!" - James Wright');
   } else {
     var main = document.getElementById('region-main');
-    if(!main) {
+    if (!main) {
       return;
     }
     main.className = "span9 pull-right";
     var side = document.getElementById('block-region-side-pre');
     side.className = "span3 desktop-first-column decaf-border decaf-border-right block-region";
-    var header = document.getElementsByClassName('navbar')[0];
-    header.className = "navbar navbar-static-top moodle-has-zindex";
   }
 });
