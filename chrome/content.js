9f348ff16d65028eff50b7941acc17af715e74d4
sha162 <sha162@uclive.ac.nz>
2018-07-18 13:22:28 +1200

    - added null check for ignored courses

--- chrome/content.js
+++ chrome/content.js
@@ -26,12 +26,15 @@
         buttons[0].click();
       }
     }
-    data.ignored_courses && data.ignored_courses.forEach((title) => {
-      let node = document.querySelector('.block_course_list [title="' + title + '"]');
-      if (node !== null) {
-        node.parentNode.parentNode.remove();
-      }
-    });
+    if (data.ignored_courses){
+        data.ignored_courses.forEach((title) => {
+            let node = document.querySelector('.block_course_list [title="' + title + '"]');
+            if (node !== null) {
+                node.parentNode.parentNode.remove();
+            }
+        });
+    }
+
   });
   let main = document.getElementById('region-main');
   if (main) {
